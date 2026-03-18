21730da8f9e1266637c1d0fcdb9c2e479ac1abdd
rochac2lee <cleberlee.rocha@gmail.com>
2026-03-18 15:31:33 -0300

    Refactor User model to remove UUID support and add new relationships with App, Niche, and Country models. Updated fillable attributes to include avatar and photography studio.

--- account/app/Models/User.php
+++ account/app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,40 +9,45 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, HasUuids, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
+        'id',
         'name',
+        'avatar',
         'email',
         'password',
+        'photography_studio',
+        'surname',
+        'instagram',
+        'country_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function apps()
+    {
+        return $this->belongsToMany(App::class)->withTimestamps();
+    }
+
+    public function niches()
+    {
+        return $this->belongsToMany(Niche::class)->withTimestamps();
+    }
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
     }
 }
